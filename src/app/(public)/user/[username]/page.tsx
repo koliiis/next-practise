e74ff99949ca8bff8902f569b9674b45290c33b4
koliiis <ann27052005@gmail.com>
2025-08-04 18:36:41 +0300

    fixed params2

--- src/app/(public)/user/[username]/page.tsx
+++ src/app/(public)/user/[username]/page.tsx
@@ -2,13 +2,11 @@ import ProfilePageClient from '@/components/profile/ProfilePage';
 import { prisma } from '@/lib/prisma';
 import { notFound } from 'next/navigation';
 
-interface PageProps {
-  params: {
-    username: string;
-  };
-}
-
-export default async function ProfilePage({ params }: PageProps) {
+export default async function ProfilePage({
+  params,
+}: {
+  params: { username: string };
+}) {
   const { username } = await params;
 
   const user = await prisma.user.findUnique({
